9a7cd040d341359ec421b0b26106177ec6967199
Felicia Hetman <feli5659@stud.kea.dk>
2022-08-23 13:01:31 +0200

    oevelser

---
+++ script.js
@@ -0,0 +1,25 @@
+const colaKnap = document.querySelector("#colaElement");
+const fantaKnap = document.querySelector("#fantaElement");
+const snapsKnap = document.querySelector("#snapsElement");
+const beerKnap = document.querySelector("#beerElement");
+
+this .addEventListener("click", colaKlik);
+// fantaKnap.addEventListener("click", fantaKlik);
+// snapsKnap.addEventListener("click", snapsKlik);
+// beerKnap.addEventListener("click", beerKlik);
+
+function colaKlik() {
+  console.log("alkoholfri");
+}
+function fantaKlik() {
+  console.log("alkoholfri");
+}
+function snapsKlik() {
+  console.log("indeholder alkohol");
+}
+
+function beerKlik() {
+  console.log("indeholder alkohol");
+}
+
+
