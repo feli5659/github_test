f1be824cfb8e330440f19e380fa04e2f9ac4169a
Felicia Hetman <feli5659@stud.kea.dk>
2022-08-24 18:30:41 +0200

    indhold

---
+++ 02-js-dom/script2.js
@@ -0,0 +1,9 @@
+const titel = document.querySelector("header h1");
+const article1 = document.querySelector("article:nth-child(1)");
+const img1 = document.querySelector("article:nth-child(1) img");
+const article1Heading = document.querySelector("article:nth-child(1) h3");
+const article1Content = document.querySelector("article:nth-child(1) p");
+
+titel.textContent = "Billeder";
+article1Heading.textContent = "Arkitektur";
+article1Content = "Bygninger skaber rum";
