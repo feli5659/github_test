f91f3e6db62e70397c5bb22c7a6fe85183b0c6c4
Felicia Hetman <feli5659@stud.kea.dk>
2022-08-24 18:35:51 +0200

    select

---
+++ 02_select/script1.js
@@ -0,0 +1,6 @@
+const titel = document.querySelector("header h1");
+const article1 = document.querySelector("article:nth-child(1)");
+const img1 = document.querySelector("article:nth-child(1) img");
+const article1Heading = document.querySelector("article:nth-child(1) h3");
+const article1Content = document.querySelector("article:nth-child(1) p");
+console.log(article1, article1Heading, img1, article1Content);
